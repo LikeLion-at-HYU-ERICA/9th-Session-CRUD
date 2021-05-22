5c1c1b17242bb711ab4029a159980dad08246131
peanutyumyum <jrimit@gmail.com>
2021-05-23 02:18:35 +0900

    feat: Add session document

---
+++ LIKELION9/myProject/mainApp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+    objects = models.Manager()
+    title = models.CharField(max_length=100)
+    writer = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    
+    # deployed at admin page
+    def __str__(self):
+        return self.title
